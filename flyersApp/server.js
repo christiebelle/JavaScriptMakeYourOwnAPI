665e9914f9302e64a0194b82cc2eb758713edfe0
Christiebelle <cslester@outlook.com>
2018-04-18 10:32:39 +0100

    connection to the db established via server

--- flyersApp/server.js
+++ flyersApp/server.js
@@ -9,3 +9,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 const MongoClient = require("mongodb").MongoClient; //allows the connection to the database
 
 //ALL OF THE ABOVE IS THE SETUP STUFF
+MongoClient.connect("mongodb://localhost:27017", function(err, db){
+  if(err){           //port 27017 is a mongo default!!
+    console.log(err);
+    return;   //essentially, log the error and then get the hell out
+  }
+
+  app.get("/", function(req, res){  //looks like it does in Java!
+    res.json("ok");
+  });
+
+  app.listen(3000, function(){ //port 3000 is a node default!!
+    console.log("Listening for requests on port 3000");
+  });
+});
