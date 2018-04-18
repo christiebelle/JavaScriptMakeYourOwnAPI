00d84a890877bea8db926381b52e31e1f14bacc7
Christiebelle <cslester@outlook.com>
2018-04-18 11:55:13 +0100

    app file added and set up

--- flyersApp/client/build/bundle.js
+++ flyersApp/client/build/bundle.js
@@ -67,8 +67,24 @@
 /* 0 */
 /***/ (function(module, exports) {
 
+const app = function(){
+  makeRequest("/players", playersRequestComplete)
+};
+
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+};
+
+const playersRequestComplete = function(){
+  console.log("Request complete! 🏒");
+  debugger;
+};
+
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("webpack is working 👾");
+  app();
 });
 
 
--- flyersApp/seeds.js
+++ flyersApp/seeds.js
@@ -15,4 +15,44 @@ db.players.insert([{
   name: "Brian Elliott",
   position: "Goalie",
   jerseyNo: 37
+},
+{
+  name: "Wayne Simmonds",
+  position: "Right Wing",
+  jerseyNo: 17
+},
+{
+  name: "Jake Voracek",
+  position: "Right Wing",
+  jerseyNo: 93
+},
+{
+  name: "Nolan Patrick",
+  position: "Centre",
+  jerseyNo: 19
+},
+{
+  name: "Travis Sanheim",
+  position: "Defence",
+  jerseyNo: 6
+},
+{
+  name: "Petr Mrazek",
+  position: "Goalie",
+  jerseyNo: 34
+},
+{
+  name: "Oskar Lindblom",
+  position: "Left Wing",
+  jerseyNo: 54
+},
+{
+  name: "Shayne Gostisbehere",
+  position: "Defence",
+  jerseyNo: 53
+},
+{
+  name: "Sean Couturier",
+  position: "Centre",
+  jerseyNo: 14
 }])
